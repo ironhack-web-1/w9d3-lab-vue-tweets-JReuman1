84b3d658ae4f31dd7396d06710aa3061301dcf1f
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-09-03 15:33:08 +0200

    finish iteration 1, 2 and 3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="app">
-    <Tweet />
+    <Tweet
+    v-for="(tweet, index) in tweets"
+    :key="index"
+    :name="tweet.user.name"
+    :image="tweet.user.image"
+    :handle="tweet.user.handle"
+    :timestamp="tweet.timestamp"
+    :message="tweet.message"
+    >
+    </Tweet>
   </div>
 </template>
 
